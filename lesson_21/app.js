5fd9bd9b2a3bcee1a851c9f45581fefbbfde9166
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2023-09-24 15:24:01 +0300

    in progress

--- lesson_21/app.js
+++ lesson_21/app.js
@@ -1,33 +1,34 @@
 // 1. Decorator 
-
-function sumF (num){
+const obj = {
+  num: 1,
+  sum(num) {
     return this.num + num;
-  };
-
-let cache = {};
-
-function cachesDecorator (func){
-    return function () {
-        return  cache[func.name] =  func.call(this, ...arguments); // .name - returns name of function
-    }
+  },
 };
-  
-  const obj = {
-    num: 1,
-    result: null,
-    sum(num) {
-      this.result = this.num + num;
-      return this.result;
-    },
-  };
 
-const decoratedSum = cachesDecorator(sumF);
+const cachesDecorator = (func) => {
+ let cache = new Map(); // create new collection (key, val)
+ return function(num){ 
+  if(!cache.has(num)){ //if collection is empty from argument of function call func
+    let result = func.call(this, num); // put the result of function that calls with object and argument num to veriale result 
+    cache.set(num, result); // set properties to cache collection num - key, result- value
+  }
+  return cache.get(num); // getting valut 'result' by key 'num'
+ }
+};
+
+const sum = function (num) {
+  return this.num + num;
+};
+
+const decoratedSum = cachesDecorator(sum);// call sum func
+console.log(decoratedSum.call(obj, 2)); // arguments in 'let result = func.call(this, num);'
 console.log(decoratedSum.call(obj, 2));
-// 3
-  
-const decoratedSum1 = cachesDecorator(obj.sum);
-console.log(decoratedSum1.call(obj, 3));
-  // 4
+
+const decoratedSum2 = cachesDecorator(obj.sum); // call method sum of obj
+console.log(decoratedSum.call(obj, 3)); // arguments in 'let result = func.call(this, num);'
+console.log(decoratedSum.call(obj, 3));
+console.log(decoratedSum.call(obj, 33));
 
 // 2. Task#2 Factorial recursion
 function factorial (initialNumber){
@@ -87,3 +88,81 @@ function readList (list){
 }
 
 console.log(readList(list));
+
+
+// 5. Deep copy
+const arr = [
+  1,
+  "string",
+  null,
+  undefined,
+  { a: 15, b: 10, c: [1, 2, 3, { a: 4 }], d: undefined, e: true },
+  true,
+  false,
+];
+
+const deepCopy = (arr) => {
+};
+let copy = deepCopy(arr);
+console.log(copy(arr));
+
+
+// 6. DOM
+const table = {
+  tagName: "table",
+  attrs: {
+    border: "1",
+  },
+  children: [
+    {
+      tagName: "tr",
+      children: [
+        {
+          tagName: "td",
+          children: ["1x1"],
+        },
+        {
+          tagName: "td",
+          children: ["1x2"],
+        },
+      ],
+    },
+    {
+      tagName: "tr",
+      children: [
+        {
+          tagName: "td",
+          children: ["2x1"],
+        },
+        {
+          tagName: "td",
+          children: ["2x2"],
+        },
+      ],
+    },
+  ],
+};
+
+const render = ({ tagName, attrs, children }) => {
+  let html = `<${tagName}`;
+  for (const attr in attrs) {
+    html += ` ${attr}="${attrs[attr]}"`;
+  }
+  html += ">";
+
+  for (const child of children) {
+    if (child?.tagName) {
+      html += render(child);
+    } else if (typeof child === "string") {
+      html += child;
+    }
+  }
+
+  html += `</${tagName}>`;
+
+  return html;
+};
+
+const html = render(table);
+
+document.write(html);
