b72995f28d824e850964d381d2d7d1ca263042c2
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2023-10-16 17:00:46 +0300

    rendered table by using js

--- codewars/app.js
+++ codewars/app.js
@@ -8,7 +8,7 @@ function createImg(src, alt) {
 //'MIDpdacjifSU395EcnZAHn79VQE9eBWwNLyGOHkV'
 
 
-async function loadImg(){
+async function loadImg() {
     let url = new URL('https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?');
     url.searchParams.set('sol', 900);
     url.searchParams.set('camera', 'pancam');
@@ -16,13 +16,86 @@ async function loadImg(){
     let resp = await fetch(url);
     let convertedData = await resp.json();
     let src = '';
-    for(let i = 0; i < 5; i++){
+    for (let i = 0; i < 6; i++) {
         src = convertedData.photos[i].img_src;
         createImg(src, 'PanCam')
     }
     //let imgSrc = convertedData.photos[2].img_src;
-    
+
 
 }
 
-loadImg();
+//loadImg();
+
+
+const tableArr = [
+    { header: "Name", data: ["Taras", "Yana"] },
+    { header: "Age", data: [25, 25] },
+    { header: "Learn Js", data: [true, true] },
+];
+
+// function createTable() {
+//     let table = document.createElement('table');
+//     let thead = document.createElement('thead');
+//     let tbody = document.createElement('tbody');
+//     table.style.border = '1px solid black';
+
+//     tableArr.forEach((el) => {
+//         let th = document.createElement('th');
+//         th.innerText = el.header;
+//         th.style.border = '1px solid black';
+//         thead.append(th);
+//     });
+
+//     for(let i = 0; i < tableArr[0].data.length; i++){
+//         let row = document.createElement('tr');
+        
+//             tableArr.forEach( (el) => {
+//             let td = document.createElement('td');
+//             td.innerText = el.data[i];
+//             td.style.border = '1px solid black';
+//             row.append(td);
+            
+//         });
+//         tbody.append(row)
+//     }
+    
+//     table.append(thead, tbody);
+//     document.body.append(table);
+
+// }
+
+
+function createTable(){
+    const table = document.createElement('table');
+    const tbody = document.createElement('tbody');
+    const thead = document.createElement('thead');
+    table.style.border = '1px solid black';
+    tableArr.forEach( (el) => {
+        let th = document.createElement('th');
+        th.innerText = el.header;
+        th.style.border = '1px solid black'
+        thead.append(th);
+    })
+
+    let rowLength = tableArr[0].data.length;
+
+    for(let i = 0; i < rowLength; i++){
+        let tr = document.createElement('tr');
+        tableArr.forEach( (el) => {
+            let td = document.createElement('td');
+            td.innerText = el.data[i];
+            td.style.border = '1px solid black';
+            if([i] % 2 == 0){
+                td.style.backgroundColor = 'grey'
+            }
+            tr.append(td);
+        })
+        tbody.append(tr);
+    }
+
+    table.append(thead, tbody);
+    document.body.append(table);
+}
+
+createTable();
